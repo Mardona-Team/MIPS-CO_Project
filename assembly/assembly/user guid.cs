9c167ecab3b45e2edb4c42ab11d1ef95d8a26ad0
ahmedanwer <superahmedawa@gmail.com>
2014-12-22 20:17:01 +0200

    assembly program was modfied

---
+++ assembly/assembly/user guid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace assembly
+{
+    public partial class user_guid : Form
+    {
+        public user_guid()
+        {
+            InitializeComponent();
+        }
+
+        private void user_guid_Load(object sender, EventArgs e)
+        {
+            string curDir = Directory.GetCurrentDirectory();
+            this.webBrowser1.Url = new Uri(String.Format("file:///{0}/user_guid.htm", curDir));
+        }
+    }
+}
